ae731fc49cdea8fbd07bd2ba9b517a9b9ba3ccf4
Petr Zajkas <petrzajkas@gmail.com>
2024-03-02 18:16:45 +0000

    page section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,22 @@
-
+import Image from "next/image";
+import image5 from "../public/assets/image5.jpg";
+import Container from "@/components/ui/container";
 export default function Home() {
   return (
-    <div>
-      Content
-    </div>
+    <Container>
+      <div className="grid items-place-center w-full">
+        <Image src={image5} alt="Gym header image" placeholder="blur" />
+        <div>
+          <h1 className="text-4xl font-bold text-center">
+            <span className="text-gray-900 dark:text-gray-100">
+              Newport MMA
+            </span>
+          </h1>
+          <h2 className="text-2xl font-bold text-center">
+            <span className="text-gray-900 dark:text-gray-100">Gym</span>
+          </h2>
+        </div>
+      </div>
+    </Container>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
   ];
 
   return (
-    <header className="sm:flex sm:justify-between py-3 px-4 border-b">
+    <header className="sm:flex sm:justify-between py-3 px-4">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between w-full">
           <div className="flex items-center">
